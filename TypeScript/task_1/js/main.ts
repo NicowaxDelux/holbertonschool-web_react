e7455a3711a4aead8cf98e6cd6ff27fd03b11dbd
NicowaxDelux <nycko111995@gmail.com>
2024-09-27 15:55:25 -0500

    Let's build a Teacher interface

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,10 +1,14 @@
 interface Teacher {
-    readonly firstName: string,
-    readonly lastName: string,
-    fullTimeEmployee: boolean,
-    yearsOfExperience?: number,
-    location: string,
-    [key: string]: any,
+    readonly firstName: string
+    readonly lastName: string
+    fullTimeEmployee: boolean
+    yearsOfExperience?: number
+    location: string
+    [key: string]: any
+}
+
+interface Directors extends Teacher {
+    numberOfReports: number
 }
 
 const teacher3: Teacher = {
@@ -16,3 +20,12 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
